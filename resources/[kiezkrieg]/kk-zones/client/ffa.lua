8a04b924296f096ac25635138072bb72b16bbd11
spachtler53 <spachtler53@gmail.com>
2025-09-05 15:29:06 +0200

    Update ffa.lua to use native FiveM notifications

--- resources/[kiezkrieg]/kk-zones/client/ffa.lua
+++ resources/[kiezkrieg]/kk-zones/client/ffa.lua
@@ -1,12 +1,9 @@
--- Updated notification function
-local function ShowNotification(message)
-    -- Using native FiveM notification
-    SetNotificationTextEntry('STRING')
-    AddTextComponentString(message)
-    DrawNotification(false, true)
+local function showNotification(message)
+    -- Replace this with the native FiveM notification method
+    TriggerEvent('chat:addMessage', { args = { message } })
 end
 
--- Example of usage
-ShowNotification('Your notification message here')
+-- Example usage of the notification function
+showNotification('Hello, world!')
 
--- Rest of the ffa.lua content goes here...
+-- Original content goes here...
